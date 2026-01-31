c5f07a4cb2117e15838766a9c86b5c07179330de
Jeffery Ye <s.jeffery.ye@gmail.com>
2026-01-31 14:52:02 -0800

    new star, finally happy with it

--- frontend/src/components/galaxy/StarIcon.jsx
+++ frontend/src/components/galaxy/StarIcon.jsx
@@ -18,13 +18,13 @@ const StarIcon = ({
                         </feMerge>
                     </filter>
                     <radialGradient id="star-radial" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
-                        <stop offset="0%" stopColor="white" />
-                        <stop offset="30%" stopColor={color} />
+                        <stop offset="20%" stopColor="white" />
+                        <stop offset="60%" stopColor={color} />
                         <stop offset="100%" stopColor={secondaryColor} />
                     </radialGradient>
                 </defs>
                 <path
-                    d="M50 0 L58 35 L100 50 L58 65 L50 100 L42 65 L0 50 L42 35 Z"
+                    d="M50 0 L62 38 L100 50 L62 62 L50 100 L38 62 L0 50 L38 38 Z"
                     fill="url(#star-radial)"
                     filter="url(#star-glow)"
                 />
@@ -46,13 +46,13 @@ const StarIcon = ({
                         </feMerge>
                     </filter>
                     <radialGradient id="project-radial" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
-                        <stop offset="0%" stopColor="white" />
-                        <stop offset="35%" stopColor={secondaryColor} />
+                        <stop offset="20%" stopColor="white" />
+                        <stop offset="60%" stopColor={secondaryColor} />
                         <stop offset="100%" stopColor="#7C3AED" />
                     </radialGradient>
                 </defs>
                 <path
-                    d="M50 5 L58 38 L95 50 L58 62 L50 95 L42 62 L5 50 L42 38 Z"
+                    d="M50 5 L60 40 L95 50 L60 60 L50 95 L40 60 L5 50 L40 40 Z"
                     fill="url(#project-radial)"
                     filter="url(#project-glow)"
                 />
@@ -75,18 +75,18 @@ const StarIcon = ({
                     </filter>
                     {/* Each star in cluster gets its own radial gradient centered on itself */}
                     <radialGradient id="cluster-main" cx="50%" cy="50%" r="50%">
-                        <stop offset="0%" stopColor="white" />
-                        <stop offset="25%" stopColor="#EC4899" />
+                        <stop offset="15%" stopColor="white" />
+                        <stop offset="55%" stopColor="#EC4899" />
                         <stop offset="100%" stopColor={secondaryColor} />
                     </radialGradient>
                     <radialGradient id="cluster-secondary" cx="50%" cy="50%" r="50%">
-                        <stop offset="0%" stopColor="white" />
-                        <stop offset="30%" stopColor={color} />
+                        <stop offset="15%" stopColor="white" />
+                        <stop offset="55%" stopColor={color} />
                         <stop offset="100%" stopColor="#0EA5E9" />
                     </radialGradient>
                     <radialGradient id="cluster-tertiary" cx="50%" cy="50%" r="50%">
-                        <stop offset="0%" stopColor="white" />
-                        <stop offset="30%" stopColor={secondaryColor} />
+                        <stop offset="15%" stopColor="white" />
+                        <stop offset="55%" stopColor={secondaryColor} />
                         <stop offset="100%" stopColor="#7C3AED" />
                     </radialGradient>
                 </defs>
@@ -94,7 +94,7 @@ const StarIcon = ({
                 {/* Main large star (center-right) */}
                 <g filter="url(#cluster-glow)">
                     <path
-                        d="M60 20 L66 42 L90 50 L66 58 L60 80 L54 58 L30 50 L54 42 Z"
+                        d="M60 20 L67 43 L90 50 L67 57 L60 80 L53 57 L30 50 L53 43 Z"
                         fill="url(#cluster-main)"
                     />
                 </g>
@@ -102,7 +102,7 @@ const StarIcon = ({
                 {/* Secondary star (top-left) */}
                 <g filter="url(#cluster-glow)">
                     <path
-                        d="M28 10 L32 25 L48 30 L32 35 L28 52 L24 35 L8 30 L24 25 Z"
+                        d="M28 11 L33 26 L48 31 L33 36 L28 51 L23 36 L8 31 L23 26 Z"
                         fill="url(#cluster-secondary)"
                     />
                 </g>
@@ -110,7 +110,7 @@ const StarIcon = ({
                 {/* Tertiary star (bottom-left) */}
                 <g filter="url(#cluster-glow)">
                     <path
-                        d="M35 58 L39 68 L50 72 L39 76 L35 88 L31 76 L20 72 L31 68 Z"
+                        d="M35 58 L39 69 L50 73 L39 77 L35 88 L31 77 L20 73 L31 69 Z"
                         fill="url(#cluster-tertiary)"
                     />
                 </g>
